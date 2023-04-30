82dc3767a34c2f73fd6876806ce75ca674949240
Sajjad C Kareem <sajjadck04@gmail.com>
2023-04-30 11:12:24 -0700

    Print null-terminated string

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,10 +1,19 @@
+[org 0x7c00]
 mov ah, 0x0e ; teletype BIOS routine
 
-mov al, 'H' ; character to print
-int 0x10 ; interrupt 0x10
+loop:
+    mov al, [my_string + bx] ; get character from string
+    cmp al, 0 ; check if null terminator
+    je done ; if so, we're done
+    int 0x10 ; print character
+    inc bx ; increment index
+    jmp loop ; repeat
 
-jmp $ ; jump to current address (infinite loop)
+done:
+    jmp $ ; infinite loop
+
+my_string:
+    db 'Booting OS', 0 ; string with null terminator
 
 times 510-($-$$) db 0 ; pad with zeros until 510 bytes (last 2 bytes are for 0xaa55)
-
 dw 0xaa55 ; define word
