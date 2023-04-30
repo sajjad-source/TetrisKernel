194935da1beba23fc356622019a691e1568827d0
Sajjad C Kareem <sajjadck04@gmail.com>
2023-04-30 11:28:01 -0700

    Print with function

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,16 +1,19 @@
-[org 0x7c00]
+[org 0x7c00] ; set origin to 0x7c00
 mov ah, 0x0e ; teletype BIOS routine
 
-loop:
-    mov al, [my_string + bx] ; get character from string
-    cmp al, 0 ; check if null terminator
-    je done ; if so, we're done
-    int 0x10 ; print character
-    inc bx ; increment index
-    jmp loop ; repeat
+call print_string ; print string
+jmp $
 
-done:
-    jmp $ ; infinite loop
+print_string:
+    loop:
+        mov al, [my_string + bx] ; get character from string
+        cmp al, 0 ; check if null terminator
+        je end ; go to end
+        int 0x10 ; print character
+        inc bx ; increment index
+        jmp loop ; repeat
+    end:
+        ret
 
 my_string:
     db 'Booting OS', 0 ; string with null terminator
