0ee166420340db12d9aceaed911b7bcba7c4cd08
Sajjad C Kareem <sajjadck04@gmail.com>
2023-05-10 21:51:32 -0700

    Added ability to modify gravity tick rate during gameplay for debug purposes

--- src/tetris/game.rs
+++ src/tetris/game.rs
@@ -7,7 +7,7 @@ pub const WIDTH: usize = 10;
 pub const HEIGHT: usize = 20;
 
 pub fn run() {
-    let mut grav_tick: usize = 100;
+    let mut grav_tick: usize = 250;
 
     let mut gs = GameState::new();
     let mut prev_scancode = 0; // required for key repeat
@@ -49,6 +49,7 @@ pub fn run() {
             key,
             &mut gs.active_piece,
             &mut gs.next_piece,
+            &mut grav_tick,
         );
 
         // hold piece
@@ -62,13 +63,10 @@ pub fn run() {
         }
 
         // full line
-        full_line(&mut gs.display, &mut gs.gamescore);
+        full_line(&mut gs.display, &mut gs.gamescore, &mut grav_tick);
 
         // ghost piece
         ghost_piece(&mut gs.display, &mut gs.active_piece);
-
-        // update grav_tick
-        grav_tick = 250 - gs.gamescore.level * 8;
 
         // check if gs.display was changed
         let is_updated = gs != prev_gs || gs.is_game_over;
@@ -80,6 +78,7 @@ pub fn run() {
             &mut gs.gamescore,
             &gs.hold_piece,
             &gs.next_piece,
+            &grav_tick,
         );
         WRITER.lock().flush();
         gs.counter += 1;
